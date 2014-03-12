6f7b0bff010954f8292a2b9d3f51def6802c3bee
Tom Diggelmann <tdiggelm@gmail.com>
2014-03-12 18:16:31 +0100

    update

--- lisp.c
+++ lisp.c
@@ -18,7 +18,7 @@ typedef enum {NDT_TYPE_PAIR, NDT_TYPE_DECIMAL, NDT_TYPE_INTEGER, NDT_TYPE_SYMBOL
 
 typedef struct object {
     NDT_TYPE type;
-} OBJECT;
+} NDT_OBJECT;
 
 typedef struct ndt_decimal {
     NDT_TYPE type;
@@ -42,88 +42,88 @@ typedef struct ndt_symbol {
 
 typedef struct pair {
     NDT_TYPE type;
-    const OBJECT* ndt_car;
-    const OBJECT* ndt_cdr;
+    const NDT_OBJECT* ndt_car;
+    const NDT_OBJECT* ndt_cdr;
 } PAIR;
 
-OBJECT* ndt_make_integer(long long ndt_integer)
+NDT_OBJECT* ndt_make_integer(long long ndt_integer)
 {
     INTEGER* obj = malloc(sizeof(INTEGER));
     obj->type = NDT_TYPE_INTEGER;
     obj->ndt_integer = ndt_integer;
-    return (OBJECT*)obj;
+    return (NDT_OBJECT*)obj;
 }
 
-OBJECT* ndt_make_symbol(const char* ndt_symbol)
+NDT_OBJECT* ndt_make_symbol(const char* ndt_symbol)
 {
     SYMBOL* obj = malloc(sizeof(SYMBOL));
     obj->type = NDT_TYPE_SYMBOL;
     obj->ndt_symbol = strdup(ndt_symbol);
-    return (OBJECT*)obj;
+    return (NDT_OBJECT*)obj;
 }
 
-OBJECT* ndt_make_string(const char* ndt_string)
+NDT_OBJECT* ndt_make_string(const char* ndt_string)
 {
     STRING* obj = malloc(sizeof(STRING));
     obj->type = NDT_TYPE_STRING;
     obj->ndt_string = strdup(ndt_string);
-    return (OBJECT*)obj;
+    return (NDT_OBJECT*)obj;
 }
 
-OBJECT* ndt_make_decimal(double ndt_decimal)
+NDT_OBJECT* ndt_make_decimal(double ndt_decimal)
 {
     DECIMAL* obj = malloc(sizeof(DECIMAL));
     obj->type = NDT_TYPE_DECIMAL;
     obj->ndt_decimal = ndt_decimal;
-    return (OBJECT*)obj;
+    return (NDT_OBJECT*)obj;
 }
 
-OBJECT* ndt_make_cons(const OBJECT* ndt_car, const OBJECT* ndt_cdr)
+NDT_OBJECT* ndt_make_cons(const NDT_OBJECT* ndt_car, const NDT_OBJECT* ndt_cdr)
 {
     PAIR* obj = malloc(sizeof(PAIR));
     obj->type = NDT_TYPE_PAIR;
     obj->ndt_car = ndt_car;
     obj->ndt_cdr = ndt_cdr;
-    return (OBJECT*)obj;
+    return (NDT_OBJECT*)obj;
 }
 
-long long ndt_integer(const OBJECT* obj)
+long long ndt_integer(const NDT_OBJECT* obj)
 {
     assert(obj->type == NDT_TYPE_INTEGER);
     return ((INTEGER*)obj)->ndt_integer;
 }
 
-double ndt_decimal(const OBJECT* obj)
+double ndt_decimal(const NDT_OBJECT* obj)
 {
     assert(obj->type == NDT_TYPE_DECIMAL);
     return ((DECIMAL*)obj)->ndt_decimal;
 }
 
-const char* ndt_symbol(const OBJECT* obj)
+const char* ndt_symbol(const NDT_OBJECT* obj)
 {
     assert(obj->type == NDT_TYPE_SYMBOL);
     return ((SYMBOL*)obj)->ndt_symbol;
 }
 
-const char* ndt_string(const OBJECT* obj)
+const char* ndt_string(const NDT_OBJECT* obj)
 {
     assert(obj->type == NDT_TYPE_STRING);
     return ((STRING*)obj)->ndt_string;
 }
 
-const OBJECT* ndt_car(const OBJECT* obj)
+const NDT_OBJECT* ndt_car(const NDT_OBJECT* obj)
 {
     assert(obj->type == NDT_TYPE_PAIR);
     return ((PAIR*)obj)->ndt_car;
 }
 
-const OBJECT* ndt_cdr(const OBJECT* obj)
+const NDT_OBJECT* ndt_cdr(const NDT_OBJECT* obj)
 {
     assert(obj->type == NDT_TYPE_PAIR);
     return ((PAIR*)obj)->ndt_cdr;
 }
 
-void ndt_release(OBJECT* obj)
+void ndt_release(NDT_OBJECT* obj)
 {
     if (obj == NULL) {
         return;
@@ -151,7 +151,7 @@ void ndt_release(OBJECT* obj)
     free((void*)obj);
 }
 
-void print(const OBJECT* obj)
+void print(const NDT_OBJECT* obj)
 {
     if (obj == NULL) {
         printf("nil");
@@ -212,16 +212,28 @@ void print(const OBJECT* obj)
     }
 }*/
 
+NDT_OBJECT* ndt_sum(const NDT_OBJECT* args)
+{
+    long long sum = 0;
+    // recusively loop args
+    return ndt_make_integer(sum);
+}
+
 int main()
 {
     size_t i;
     //for (i = 0; i < 10000; i++)
     {
-        OBJECT* obj = ndt_make_cons(ndt_make_symbol("list"), ndt_make_cons(ndt_make_string("Hello World!"), ndt_make_cons(ndt_make_decimal(3.14159), ndt_make_cons(ndt_make_integer(100), NULL))));
+        NDT_OBJECT* obj = ndt_make_cons(ndt_make_symbol("list"), ndt_make_cons(ndt_make_string("Hello World!"), ndt_make_cons(ndt_make_decimal(3.14159), ndt_make_cons(ndt_make_integer(100), NULL))));
         print(obj);
         printf("\n");
         ndt_release(obj);
     }
+    
+    NDT_OBJECT* ret = ndt_sum(ndt_make_cons(ndt_make_integer(10), ndt_make_cons(ndt_make_integer(20), NULL)));
+    print(ret);
+    printf("\n");
+    ndt_release(ret);
     
     /*obj arr[] = {
         {.type = NDT_TYPE_NIL},
