d543ead0072aaef24ae573aa8ce6e1016b2a239c
Tom Diggelmann <tdiggelm@gmail.com>
2014-03-19 16:37:41 +0100

    added test for Boehm garbage collector

--- Makefile
+++ Makefile
@@ -2,15 +2,20 @@ FILES = scheme.c
 CC = gcc
 LDFLAGS = -std=gnu99
 
+all: scheme test-gc
+
 scheme: $(FILES)
 	$(CC) $(LDFLAGS) -o $@ $^
 	
 valgrind: scheme
 	valgrind --leak-check=full ./scheme
 	
+test-gc: test-gc.c
+	gcc -o test-gc test-gc.c -lgc
+	
 .PHONY: clean
 clean:
-	rm -f scheme *.o
+	rm -f "test-gc" scheme *.o
 	
 .PHONY: rebuild
 rebuild: clean scheme
---
+++ test-gc.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <stdio.h>
+#include <gc/gc.h>
+ 
+// needs sudo apt-get install libgc-dev
+ 
+int main(void)
+{
+    int i;
+ 
+    GC_INIT();
+    for (i = 0; i < 10000000; ++i)
+    {
+        int **p = (int**)GC_MALLOC(sizeof(int *));
+        int *q = (int*)GC_MALLOC_ATOMIC(sizeof(int));
+ 
+        assert(*p == 0);
+        *p = (int*)GC_REALLOC(q, 2 * sizeof(int));
+        if (i % 100000 == 0)
+            printf("Heap size = %zu\n", GC_get_heap_size());
+    }
+ 
+    return 0;
+}
